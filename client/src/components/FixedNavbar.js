81520698b76892cf42fe2d7769938b14dc725e64
Max Engel <maxengel99@gmail.com>
2020-03-29 14:17:11 -0500

    Add user info to page

--- client/src/components/FixedNavbar.js
+++ client/src/components/FixedNavbar.js
@@ -1,19 +1,36 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
+import { useAuth0 } from "../auth0/react-auth0-spa";
 
-function FixedNavbar() {
+const FixedNavbar = () => {
+  const { user } = useAuth0();
+
+  const imgStyle = {
+    margin: "5px",
+    borderRadius: "50%"
+  };
+
   return (
     <div>
       <Navbar bg="light" variant="light">
         <Navbar.Brand>Learn Khmer!</Navbar.Brand>
         <Navbar.Collapse className="justify-content-end">
           <Navbar.Text>
-            Signed in as: <b>Max</b>
+            Signed in as: <b>{user.name}</b>
           </Navbar.Text>
+          <Navbar.Brand>
+            <img
+              src={user.picture}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              style={imgStyle}
+            />
+          </Navbar.Brand>
         </Navbar.Collapse>
       </Navbar>
     </div>
   );
-}
+};
 
 export default FixedNavbar;
