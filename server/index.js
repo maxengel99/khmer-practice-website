f253c38fe91144bb64ea1f0d54dbe3bb84b367cf
Max Engel <maxengel99@gmail.com>
2020-03-30 15:01:26 -0500

    Express on firebase

--- server/index.js
+++ server/index.js
@@ -1,3 +1,24 @@
+const admin = require("firebase-admin");
+const functions = require("firebase-functions");
+const express = require("express");
+const path = require("path");
+var firebase = require("firebase");
+const bodyParser = require("body-parser");
+
+admin.initializeApp(functions.config().firebase);
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get("/api/getList", (req, res) => {
+  var list = ["item1", "item2", "item3"];
+  res.json(list);
+  console.log("Sent list of items");
+});
+
+exports.app = functions.https.onRequest(app);
+/*const functions = require("firebase-functions");
 const express = require("express");
 const path = require("path");
 var firebase = require("firebase");
@@ -24,11 +45,15 @@ var config = {
 firebase.initializeApp(config);
 
 // An api endpoint that returns a short list of items
-app.get("/api/getList", (req, res) => {
+const testApp = express();
+
+testApp.get("/api/getList", (req, res) => {
   var list = ["item1", "item2", "item3"];
   res.json(list);
   console.log("Sent list of items");
 });
+
+const testApi = functions.https.onRequest(testApp);
 
 app.get("/media", (req, res) => {
   console.log("getting media file");
@@ -61,6 +86,10 @@ app.get("*", (req, res) => {
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port);
+//app.listen(port);
+module.exports = {
+  testApi
+};
 
 console.log("App is listening on port " + port);
+*/
