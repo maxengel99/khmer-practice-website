7c222b221f3307c2662279fbfb6bceaec40cace0
Max Engel <maxengel99@gmail.com>
2020-04-03 16:33:41 -0500

    Use threading to get word list

--- Python Scripts/anki.py
+++ Python Scripts/anki.py
@@ -1,6 +1,10 @@
 import json
 import urllib.request
 import requests
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import threading
+
+lock = threading.Lock()
 
 
 def request(action, **params):
@@ -22,12 +26,51 @@ def invoke(action, **params):
     return response['result']
 
 
-result = invoke('findCards', query="deck:current")
+def check_url(card, success_word_dict, fail_word_dict):
+    url = "https://kheng.info/static/dictionary/audio/{}.mp3".format(
+        card['fields']['Khmer']['value'])
 
+    status = requests.head(url).status_code
+    try:
+        lock.acquire()
+        if status == 200:
+            success_word_dict[card['fields']['Khmer']['value']
+                              ] = card['fields']['English']['value']
+            print('added to success list {}'.format(
+                card['fields']['English']['value']))
+
+        else:
+            fail_word_dict[card['fields']['Khmer']['value']
+                           ] = card['fields']['English']['value']
+            print('added to fail list {}'.format(
+                card['fields']['English']['value']))
+
+        return card['fields']['English']['value'] + ' is done'
+    finally:
+        lock.release()
+
+
+result = invoke('findCards', query="deck:current")
 cards_info = invoke('cardsInfo', cards=result)
 
-print(cards_info[0]['fields']['Khmer']['value'])
-print(cards_info[0]['fields']['English']['value'])
+success_word_dict = {}
+fail_word_dict = {}
+word_set = {None}
+processes = []
+
+with ThreadPoolExecutor(max_workers=100) as executor:
+    for card in cards_info:
+        if(card['fields']['Khmer']['value'] in word_set):
+            print('skpping ' + card['fields']['English']['value'])
+            continue
+
+        processes.append(executor.submit(
+            check_url, card, success_word_dict, fail_word_dict))
+        word_set.add(card['fields']['Khmer']['value'])
+
+
+for task in as_completed(processes):
+    print(task.result())
 
 all_words_file = open("all-words.txt", "w+", encoding="utf8", errors="ignore")
 success_words_file = open("success-words.txt", "w+",
@@ -35,31 +78,7 @@ success_words_file = open("success-words.txt", "w+",
 fail_words_file = open("fail-words.txt", "w+",
                        encoding="utf8", errors="ignore")
 
-success_word_dict = {}
-fail_word_dict = {}
-
-for card in cards_info:
-    all_words_file.write(
-        "{}/{} ".format(card['fields']['Khmer']['value'], card['fields']['English']['value']))
-
-    url = "https://kheng.info/static/dictionary/audio/{}.mp3".format(
-        card['fields']['Khmer']['value'])
-
-    if requests.head(url).status_code == 200:
-        success_word_dict[card['fields']['Khmer']['value']
-                          ] = card['fields']['English']['value']
-        print('succesfully added {}'.format(
-            card['fields']['English']['value']))
-
-    else:
-        fail_word_dict[card['fields']['Khmer']['value']
-                       ] = card['fields']['English']['value']
-        print('succesfully added {}'.format(
-            card['fields']['Khmer']['value']))
-
-    break
-
+all_words_file.write(json.dumps(
+    {**success_word_dict, **fail_word_dict}, ensure_ascii=False))
 success_words_file.write(json.dumps(success_word_dict, ensure_ascii=False))
 fail_words_file.write(json.dumps(fail_word_dict, ensure_ascii=False))
-print(success_word_dict)
-print(fail_word_dict)
